19263e4a5c32360da29336a6652f4303627fd6be
DanielPalacz <daniel.palacz@gmail.com>
2021-01-05 21:29:24 +0100

    Progress - Added text to translate.

--- test_task.py
+++ test_task.py
@@ -14,6 +14,40 @@ from webdriver_manager.chrome import ChromeDriverManager
 import pytest
 
 
+# https://sportowefakty.wp.pl/
+# skoki-narciarskie/917575/69-turniej-czterech-skoczni-pierwsza-proba-sil-dla-kamila-stocha-co-za-skok-pola
+
+TEXT_TRANSLATE = """
+69. Turniej Czterech Skoczni. Pierwsza próba sił dla Kamila Stocha! Co za skok Polaka! Sensacyjna porażka Słoweńca.
+Halvor Egner Granerud postraszył na treningu, ale kwalifikacje w Bischofshofen wygrał już Kamil.
+Polak zwyciężył po długim pięknym stylowo skoku. Norweg zajął 3 miejsce.
+Sensacją eliminacji jest odpadnięcie Anze Laniska. Najważniejsze jednak, że Stoch wygrał próbę nerwów.
+Pokazał Norwegowi, że nie robią na nim żadnego wrażenia dalekie skoki rywala z treningu.
+Co więcej, to Granerud wydaje się być znacznie bardziej zdenerwowany.
+Gdy zdał już sobie sprawę, że nie wygra kwalifikacji, machał zrezygnowany głową.
+Widać było, że chciał we wtorek wywrzeć presję na trzykrotnym mistrzu olimpijskim, ale zupełnie mu się to nie udało.
+W eliminacjach z bardzo dobrej strony pokazał się także Dawid Kubacki.
+Rekordzista skoczni, z dość mocnym wiatrem w plecy, uzyskał 134,5 metra. Zajął 6. miejsce.
+Zwykle Kubacki w treningach w Bischofshofen jednak nie błyszczy, a wszystko co najlepsze zostawia na konkurs.
+Oby tak było także i tym razem, bo wciąż liczymy na podwójne zwycięstwo Biało-Czerwonych w turnieju.
+Sensacją kwalifikacji jest dopiero 52. miejsce Anze Laniska.
+W Innsbrucku Słoweniec przegrał tylko, że Stochem, od kilku konkursów skakał świetnie a w Bischofshofen spadł na bulę.
+108 metrów oznaczało dla niego koniec turnieju.
+Nie zakwalifikował się do pierwszej serii i nie utrzyma wysokiego 6. miejsca w całych zawodach.
+Do środowego konkursu łącznie awansowało siedmiu Polaków.
+Z dobrej strony pokazał się Aleksander Zniszczoł, który po próbie na 126. metr zajął 18. miejsce.
+Fajerwerków nie odpalili w kwalifikacjach Andrzej Stękała i Piotr Żyła sklasyfikowani na 26. i 28. lokacie.
+Żyła nie raz przyzwyczaił jednak, że w dzień kwalifikacyjny i w dzień konkursowy prezentuje zupełnie inne oblicza.
+Z piątej dziesiątki do konkursu awansowali 42. Klemens Murańka i 45. Maciej Kot. 
+Pary, w których będą skakać Biało-Czerwoni w środę, prezentują się bardzo ciekawie.
+Stękała otworzy konkurs, a jego rywalek w pierwszej parze dnia będzie sam Karl Geiger. 
+Niemiec jest 4ty w turnieju, ale w części austriackiej obniżył loty. W kwalifikacjach był 25. po skoku na 123,5 metra. 
+W eliminacjach panowały bardzo dobre warunki do skakania. Wiatr nie przekraczał 0,5 m/s. 
+Momentami wiało trochę mocniej w plecy, ale i tak - po raz kolejny w tym turnieju - na warunki nie można narzekać. 
+Wszyscy skakali z 9. belki startowej. Czekamy już na wielki finał 69. Turnieju Czterech Skoczni."""
+
+
+
 class TurboMainPage:
     """Page Object class for main page: 'https://turbotlumaczenia.pl/'"""
 
